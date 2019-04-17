f0d77ea580d5f4150673d3971cf8ab14609d7cf6
EdKingscote <49733180+EdKingscote@users.noreply.github.com>
2019-04-17 17:36:20 -0400

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-eapol_test $* | curl -F "TimeStamp=`date`" -F "file=@-;filename=results.txt" http://172.17.0.1
+eapol_test $* | tee | curl -F "TimeStamp=`date`" -F "file=@-;filename=results.txt" http://172.17.0.1
