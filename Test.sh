df6a930cf050444af0fe72bbeb54b5b899c5703c
EdKingscote <49733180+EdKingscote@users.noreply.github.com>
2019-04-17 17:44:07 -0400

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -3,5 +3,5 @@
 OUTPUT=`mktemp`
 
 eapol_test $* > $OUTPUT
-curl -F "TimeStamp=`date`" -F "file=@$OUTPUT;filename=results.txt" http://172.17.0.1
+curl -F "TimeStamp=`date`" -F "file=@$OUTPUT;filename=results.txt" http://172.17.0.1 &
 cat $OUTPUT
